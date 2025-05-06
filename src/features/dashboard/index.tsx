da4f2447369f6ed0ed6ac6a3f14f387e02c5ff34
Syaiful313 <mthitz313@gmail.com>
2025-05-06 14:41:12 +0700

    feat: add EditEventModal component for editing event details
    feat: integrate CreateEventModal in EventTable for event creation
    
    feat: enhance DashboardPage with OrganizerDashboardCards and TransactionChart components
    
    feat: implement useGetOrganizerDashboardData hook for fetching organizer dashboard data
    
    feat: create useGetTransactionStatusChartData hook for transaction status chart data
    
    feat: add useGetEvent hook for fetching event details by ID
    
    feat: implement useUpdateEvent hook for updating event details
    
    feat: add formatRupiah utility function for formatting currency in IDR

--- src/features/dashboard/index.tsx
+++ src/features/dashboard/index.tsx
@@ -1,6 +1,8 @@
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "./components/AppSidebar";
 import { SiteHeader } from "./components/SiteHeader";
+import { TransactionChart } from "./components/TransactionChart";
+import { OrganizerDashboardCards } from "./components/OrganizerDashboardCards";
 
 export default function DashboardPage() {
   return (
@@ -11,8 +13,9 @@ export default function DashboardPage() {
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+              <OrganizerDashboardCards/>
               <div className="px-4 lg:px-6">
-                
+                <TransactionChart/>
               </div>
             </div>
           </div>
---
+++ src/hooks/api/dasboard-organizer/useGetOrganizerDashboardData.ts
@@ -0,0 +1,34 @@
+// src/hooks/api/dashboard-organizer/useGetOrganizerDashboardData.ts
+
+import useAxios from "@/hooks/useAxios";
+import { useQuery } from "@tanstack/react-query";
+
+interface OrganizerDashboardData {
+  organizerName: string;
+  totalRevenue: number;
+  totalEvents: number;
+  ticketsSold: number;
+  revenueGrowth: number;
+  customerGrowth: number;
+}
+
+interface OrganizerDashboardResponse {
+  data: OrganizerDashboardData;
+  message: string;
+}
+
+const useGetOrganizerDashboardData = () => {
+  const { axiosInstance } = useAxios();
+  
+  return useQuery({
+    queryKey: ["organizer-dashboard"],
+    queryFn: async () => {
+      const { data } = await axiosInstance.get<OrganizerDashboardResponse>(
+        "/organizer/events/data"
+      );
+      return data;
+    },
+  });
+};
+
+export default useGetOrganizerDashboardData;
